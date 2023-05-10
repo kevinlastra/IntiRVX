7df127fbdfb4aa74178e6ee1ce89c1ea08944fad
kevinlastra <kevinlastramichel@gmail.com>
2023-05-10 11:16:01 +0200

    rework of the decoder on the pc gen unit

--- rtl/cpu/decoder_PG.sv
+++ rtl/cpu/decoder_PG.sv
@@ -5,257 +5,357 @@
 module decoder_PG
 (
   input logic[17:0] instruction,
-  output logic[9:0] decode
+  output logic[16:0] decode
 );
 parameter xlen = 32;
 
-logic[3:0] instr_type;
-logic[5:0] instr_code;
+
+logic[1:0] unit; //{alu, l/s, csr}
+logic[1:0] sub_unit; 
+logic[5:0] sel;
+logic imm_s;
+logic imm_l;
+
+logic csr;
+logic fence;
+logic ecall;
+logic ebreak;
+
+logic illegal_instr;
+
+// *-----------------------------------------------------------------------------------------------------*
+// |                                          TABLE INSTR DECODE                                         |
+// *---------*---------------------------------------------------------*--------------------*------------*
+// | Unit    |                         0: ALU                          |      1: L/S        |   2: CSR   |
+// *---------*------------*-----------*--------*------------*----------*---------*----------*------------*
+// | subunit | 0: Special | 1: Branch | 2: Add | 3: logique | 4: shift | 0: load | 1: store |      0     |
+// *---------*------------*-----------*--------*------------*----------*---------*----------*------------*
+// | intrs   |0  lui      |0  beq     |0 add   |0  slt      |0  sll    |0  lb    |0   sb    |0   csrrw   |
+// | intrs   |1  auipc    |1  bne     |1 sub   |1  sltu     |1  srl    |1  lh    |1   sh    |1   csrrs   |
+// | intrs   |2  jal      |2  blt     |  addi  |2  xor      |2  sra    |2  lw    |2   sw    |2   csrrc   |
+// | intrs   |3  jalr     |3  bge     |        |3  or       |   slli   |3  lbu   |          |    csrrwi  |
+// | intrs   |            |4  bltu    |        |4  and      |   srli   |4  lhu   |          |    csrrsi  |
+// | intrs   |            |5  bgeu    |        |   slti     |   srai   |         |          |    csrrci  |
+// | intrs   |            |           |        |   sltiu    |          |         |          |            |
+// | intrs   |            |           |        |   xori     |          |         |          |            |
+// | intrs   |            |           |        |   ori      |          |         |          |            |
+// | intrs   |            |           |        |   andi     |          |         |          |            |
+// *---------*------------*-----------*--------*------------*----------*---------*----------*------------*
+
 
 always  begin
+  illegal_instr = 0;
+  imm_s = 0;
+  imm_l = 0;
+  csr = 0;
+  fence = 0;
+  ecall = 0;
+  ebreak = 0;
+
   casez (instruction)
     18'b???????????0110111: // LUI
     begin
-      instr_type = 'h5;
-      instr_code = 'h0;
+      unit = 'h0;
+      sub_unit = 'h0;
+      sel = 'h0;
+      imm_l = 1;
     end
     18'b???????????0010111: // AUIPC
     begin
-      instr_type = 'h5;
-      instr_code = 'h1;
+      unit = 'h0;
+      sub_unit = 'h0;
+      sel = 'h1;
+      imm_l = 1;
     end
     18'b???????????1101111: // JAL
     begin
-      instr_type = 'h6;
-      instr_code = 'h0;
+      unit = 'h0;
+      sub_unit = 'h0;
+      sel = 'h2;
+      imm_l = 1;
     end
     18'b????????0001100111: // JALR
     begin
-      instr_type = 'h2;
-      instr_code = 'h0;
+      unit = 'h0;
+      sub_unit = 'h0;
+      sel = 'h3;
+      imm_l = 1;
     end
     18'b????????0001100011: // BEQ
     begin
-      instr_type = 'h4;
-      instr_code = 'h0;
+      unit = 'h0;
+      sub_unit = 'h1;
+      sel = 'h0;
+      imm_s = 1;
     end
     18'b????????0011100011: // BNE
     begin
-      instr_type = 'h4;
-      instr_code = 'h1;
+      unit = 'h0;
+      sub_unit = 'h1;
+      sel = 'h1;
+      imm_s = 1;
     end
     18'b????????1001100011: // BLT
     begin
-      instr_type = 'h4;
-      instr_code = 'h2;
+      unit = 'h0;
+      sub_unit = 'h1;
+      sel = 'h2;
+      imm_s = 1;
     end
     18'b????????1011100011: // BGE
     begin
-      instr_type = 'h4;
-      instr_code = 'h3;
+      unit = 'h0;
+      sub_unit = 'h1;
+      sel = 'h3;
+      imm_s = 1;
     end
     18'b????????1101100011: // BLTU
     begin
-      instr_type = 'h4;
-      instr_code = 'h4;
+      unit = 'h0;
+      sub_unit = 'h1;
+      sel = 'h4;
+      imm_s = 1;
     end
     18'b????????1111100011: // BGEU
     begin
-      instr_type = 'h4;
-      instr_code = 'h5;
+      unit = 'h0;
+      sub_unit = 'h1;
+      sel = 'h5;
+      imm_s = 1;
     end
     18'b????????0000000011: // LB
     begin
-      instr_type = 'h2;
-      instr_code = 'h1;
+      unit = 'h1;
+      sub_unit = 'h0;
+      sel = 'h0;
+      imm_s = 1;
     end
     18'b????????0010000011: // LH
     begin
-      instr_type = 'h2;
-      instr_code = 'h2;
+      unit = 'h1;
+      sub_unit = 'h0;
+      sel = 'h1;
+      imm_s = 1;
     end
     18'b????????0100000011: // LW
     begin
-      instr_type = 'h2;
-      instr_code = 'h3;
+      unit = 'h1;
+      sub_unit = 'h0;
+      sel = 'h2;
+      imm_s = 1;
     end
     18'b????????1000000011: // LBU
     begin
-      instr_type = 'h2;
-      instr_code = 'h4;
+      unit = 'h1;
+      sub_unit = 'h0;
+      sel = 'h3;
+      imm_s = 1;
     end
     18'b????????1010000011: // LHU 
     begin
-      instr_type = 'h2;
-      instr_code = 'h5;
+      unit = 'h1;
+      sub_unit = 'h0;
+      sel = 'h4;
+      imm_s = 1;
     end
     18'b????????0000100011: // SB 
     begin
-      instr_type = 'h3;
-      instr_code = 'h0;
+      unit = 'h1;
+      sub_unit = 'h1;
+      sel = 'h0;
+      imm_s = 1;
     end
     18'b????????0010100011: // SH 
     begin
-      instr_type = 'h3;
-      instr_code = 'h1;
+      unit = 'h1;
+      sub_unit = 'h1;
+      sel = 'h1;
+      imm_s = 1;
     end
     18'b????????0100100011: // SW   
     begin
-      instr_type = 'h3;
-      instr_code = 'h2;
+      unit = 'h1;
+      sub_unit = 'h1;
+      sel = 'h2;
+      imm_s = 1;
     end
     18'b????????0000010011: // ADDI 
     begin
-      instr_type = 'h2;
-      instr_code = 'h5;
+      unit = 'h0;
+      sub_unit = 'h2;
+      sel = 'h0;
+      imm_s = 1;
     end
     18'b????????0100010011: // SLTI 
     begin
-      instr_type = 'h2;
-      instr_code = 'h6;
+      unit = 'h0;
+      sub_unit = 'h3;
+      sel = 'h0;
+      imm_s = 1;
     end
     18'b????????0110010011: // SLTIU 
     begin
-      instr_type = 'h2;
-      instr_code = 'h7;
+      unit = 'h0;
+      sub_unit = 'h3;
+      sel = 'h1;
+      imm_s = 1;
     end
     18'b????????1000010011: // XORI 
     begin
-      instr_type = 'h2;
-      instr_code = 'h8;
+      unit = 'h0;
+      sub_unit = 'h3;
+      sel = 'h2;
+      imm_s = 1;
     end
     18'b????????1100010011: // ORI   
     begin
-      instr_type = 'h2;
-      instr_code = 'h9;
+      unit = 'h0;
+      sub_unit = 'h3;
+      sel = 'h3;
+      imm_s = 1;
     end
     18'b????????1110010011: // ANDI 
     begin
-      instr_type = 'h2;
-      instr_code = 'ha;
+      unit = 'h0;
+      sub_unit = 'h3;
+      sel = 'h4;
+      imm_s = 1;
     end
     18'b0000000?0010010011: // SLLI
     begin
-      instr_type = 'h1;
-      instr_code = 'h0;
+      unit = 'h0;
+      sub_unit = 'h4;
+      sel = 'h0;
+      imm_s = 1;
     end
     18'b0000000?1010010011: // SRLI
     begin
-      instr_type = 'h1;
-      instr_code = 'h1;
+      unit = 'h0;
+      sub_unit = 'h4;
+      sel = 'h1;
+      imm_s = 1;
     end
     18'b0100000?1010010011: // SRAI
     begin
-      instr_type = 'h1;
-      instr_code = 'h2;
+      unit = 'h0;
+      sub_unit = 'h4;
+      sel = 'h1;
+      imm_s = 1;
     end
     18'b0000000?0000110011: // ADD
     begin
-      instr_type = 'h1;
-      instr_code = 'h3;
+      unit = 'h0;
+      sub_unit = 'h2;
+      sel = 'h0;
     end
     18'b0100000?0000110011: // SUB
     begin
-      instr_type = 'h1;
-      instr_code = 'h4;
+      unit = 'h0;
+      sub_unit = 'h2;
+      sel = 'h1;
     end
     18'b0000000?0010110011: // SLL
     begin
-      instr_type = 'h1;
-      instr_code = 'h5;
+      unit = 'h0;
+      sub_unit = 'h4;
+      sel = 'h0;
     end
     18'b0000000?0100110011: // SLT
     begin
-      instr_type = 'h1;
-      instr_code = 'h6;
+      unit = 'h0;
+      sub_unit = 'h3;
+      sel = 'h0;
     end
     18'b0000000?0110110011: // SLTU
     begin
-      instr_type = 'h1;
-      instr_code = 'h7;
+      unit = 'h0;
+      sub_unit = 'h3;
+      sel = 'h1;
     end
     18'b0000000?1000110011: // XOR
     begin
-      instr_type = 'h1;
-      instr_code = 'h8;
+      unit = 'h0;
+      sub_unit = 'h3;
+      sel = 'h2;
     end
     18'b0000000?1010110011: // SRL
     begin
-      instr_type = 'h1;
-      instr_code = 'h9;
+      unit = 'h0;
+      sub_unit = 'h4;
+      sel = 'h1;
     end
     18'b0100000?1010110011: // SRA
     begin
-      instr_type = 'h1;
-      instr_code = 'ha;
+      unit = 'h0;
+      sub_unit = 'h4;
+      sel = 'h2;
     end
     18'b0000000?1100110011: // OR
     begin
-      instr_type = 'h1;
-      instr_code = 'hb;
+      unit = 'h0;
+      sub_unit = 'h3;
+      sel = 'h3;
     end
     18'b0000000?1110110011: // AND
     begin
-      instr_type = 'h1;
-      instr_code = 'hc;
+      unit = 'h0;
+      sub_unit = 'h3;
+      sel = 'h4;
     end
     18'b0000????0000001111: // FENCE
     begin
-      instr_type = 'h7;
-      instr_code = 'h0;
+      fence = 'b1;
     end
     18'b000000000010001111: // FENCE.I
     begin
-      instr_type = 'h7;
-      instr_code = 'h1;
+      fence = 'b1;
+      imm_s = 'b1;
     end
     18'b000000000001110011: // ECALL
     begin
-      instr_type = 'h7;
-      instr_code = 'h2;
+      ecall = 'b1;
     end
     18'b000000010001110011: // EBREAK
     begin
-      instr_type = 'h7;
-      instr_code = 'h3;
+      ebreak = 'b1;
     end
     18'b????????0011110011: // CSRRW
     begin
-      instr_type = 'h1;
-      instr_code = 'hd;
+      csr = 'b1;
+      sel = 'b0;
     end
     18'b????????0101110011: // CSRRS
     begin
-      instr_type = 'h1;
-      instr_code = 'he;
+      csr = 'b1;
+      sel = 'b1;
     end
     18'b????????0111110011: // CSRRC
     begin
-      instr_type = 'h1;
-      instr_code = 'hf;
+      csr = 'b1;
+      sel = 'b10;
     end
     18'b????????1011110011: // CSRRWI
     begin
-      instr_type = 'h1;
-      instr_code = 'h10;
+      sr = 'b1;
+      sel = 'b0;
+      imm_s = 1;
     end
     18'b????????1101110011: // CSRRSI
     begin
-      instr_type = 'h1;
-      instr_code = 'h11;
+      sr = 'b1;
+      sel = 'b1;
+      imm_s = 1;
     end
     18'b????????1111110011: // CSRRCI
     begin
-      instr_type = 'h1;
-      instr_code = 'h12;
+      sr = 'b1;
+      sel = 'b10;
+      imm_s = 1;
     end
-    default: 
-    begin
-      instr_type = 'h0;
-      instr_code = 'h0;
-    end
+    default:
+      illegal_instr = 1;
   endcase
-  decode = {instr_type, instr_code};
+  decode = {unit, sub_unit, imm_s, imm_l, csr, fence, ecall, ebreak, illegal_instr};
 end
 
 endmodule
